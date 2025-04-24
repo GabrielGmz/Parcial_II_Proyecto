a50321e7f5312f22be13861507a13d64bfb424bf
GabrielGmz <alonsogg937@gmail.com>
2025-04-24 11:29:18 -0600

    Changes

--- database/migrations/2025_04_24_170729_create_login_table.php
+++ database/migrations/2025_04_24_170729_create_login_table.php
@@ -14,6 +14,11 @@ return new class extends Migration
         Schema::create('login', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->int('id');
+            $table->string('nombre');
+            $table->string('email');
+            $table->string('password');
+            $table->string('rol');
         });
     }
 
